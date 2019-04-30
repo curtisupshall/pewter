38aaedbc2eb14d292604fb64825f025930833612
Curtis Upshall <curtisupshall@gmail.com>
2019-04-29 19:16:53 -0700

    webpack react setup

---
+++ demo/src/components/Demo.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import * as Pewter from '../../../pewter'
+
+export class Demo extends React.Component {
+	render() {
+		return (
+			<div className='demo-container'>
+				<h1>Pewter</h1>
+				<h2>Intelligent color palettes from images.</h2> 
+			</div>
+		)
+	}
+}
