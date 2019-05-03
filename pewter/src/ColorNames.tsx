b31f8323b3a4f2d0116775106734f0646edf74a5
Curtis Upshall <curtisupshall@uvic.ca>
2019-05-02 22:26:19 -0700

    Create ColorNames class manifest

---
+++ pewter/src/ColorNames.tsx
@@ -0,0 +1,14 @@
+/**
+ * @TODO Implement color names
+ */
+export default class ColorNames {
+
+    /**
+     * 
+     * @param colors An array of hexidecimal colors
+     * @return A corresponding array of color names
+     */
+    constructor(colors: string[]) {
+        return
+    }
+}
