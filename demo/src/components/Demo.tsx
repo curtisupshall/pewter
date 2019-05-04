95b4ff3cb16ba392128fd058169c6078d651cd55
Curtis Upshall <curtisupshall@uvic.ca>
2019-05-03 20:00:17 -0700

    Github link; image popover

--- demo/src/components/Demo.tsx
+++ demo/src/components/Demo.tsx
@@ -6,6 +6,9 @@ import RGB from '../../../pewter/src/RGB'
 
 import { Slider } from '@material-ui/lab'
 import {
+	Button,
+	Dialog,
+	Grow,
 	Icon,
 	Switch,
 	TextField
@@ -50,6 +53,16 @@ export class Demo extends React.Component<{}, IState> {
 		this.setState({ numColors: event.target.value })
 	}
 
+	handleFilterChange = (field: string, value: any) => {
+		this.setState((state: IState) => {
+			return { filter: { ...state.filter, [field]: value } }
+		})
+	}
+
+	handleReset = () => {
+		this.setState({ options: defaultOptions })
+	}
+
 	render() {		
 		const { tolerance, filterTolerance, threshold } = this.state.options
 		
@@ -59,7 +72,7 @@ export class Demo extends React.Component<{}, IState> {
 		image.onload = () => {
 			this.palette.setImage(image)
 		}
-		
+
 		const colors = this.palette.getColors(this.state.numColors)
 
 		return (
@@ -72,12 +85,18 @@ export class Demo extends React.Component<{}, IState> {
 						</div>
 					</li>
 					<li>
-						<a href='https://github.com/curtisupshall'>GitHub logo here</a>
+						<a href='https://github.com/curtisupshall/pewter'>
+							<img className='octocat' src='src/assets/images/octocat.svg' />
+						</a>
 					</li>
 				</ul>
 				<div>
-					<img src={this.src} />
-					<h6>{`Colors (${colors.length})`}</h6>
+					<div className='image-container'>
+						<img src={this.src} />
+						<div className='image-cover'>
+							<div className='image-cover__label'>Change Image</div>
+						</div>
+					</div>
 					<ul className='demo-colors'>
 						{colors.map((color: RGB, index: number) => (
 							<li key={index}>
@@ -105,15 +124,6 @@ export class Demo extends React.Component<{}, IState> {
 							/>
 						</li>
 						<li>
-							<h4>Filter Tolerance<span>{filterTolerance}</span></h4>
-							<Slider
-								name='filterTolerance'
-								value={filterTolerance}
-								onChange={(event: any, value: number) => this.handleValueChange('filterTolerance', value)}
-								step={1}
-							/>
-						</li>
-						<li>
 							<h4>Threshold<span>{threshold}</span></h4>
 							<Slider
 								name='threshold'
@@ -122,17 +132,40 @@ export class Demo extends React.Component<{}, IState> {
 								step={1}
 							/>
 						</li>
+						<Grow in={this.state.filter.enabled}>
+							<li>
+								<h4>Filter Tolerance<span>{filterTolerance}</span></h4>
+								<Slider
+									name='filterTolerance'
+									value={filterTolerance}
+									onChange={(event: any, value: number) => this.handleValueChange('filterTolerance', value)}
+									step={1}
+								/>
+							</li>
+						</Grow>
 					</ul>
+					<Button
+						variant='contained'
+						name='reset'
+						className='reset=button'
+						onClick={() => this.handleReset()}>Reset</Button>
 					<div className='color-options'>
 						<TextField
 							name='numColors'
 							label='Colors'
+							type='number'
 							value={this.state.numColors}
 							min={1}
 							max={8}
 							onChange={this.handleNumColorsChange}
 						/>
-						<Switch value={this.state.filter.enabled} />
+						<div>
+							<Switch
+								value={this.state.filter.enabled}
+								onChange={(event: any, value: boolean) => this.handleFilterChange('enabled', value)}
+							/>
+							<span>Filter Color</span>
+						</div>
 					</div>
 				</div>
 			</div>
