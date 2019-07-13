39382e60c281aede6dc851b275c2f028138d7b5d
Curtis Upshall <curtisupshall@uvic.ca>
2019-07-13 10:06:06 -0700

    Begin restructure

--- pewter/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var RGB_1 = require("./RGB");
-var rgb = new RGB_1.RGB(5, 10, 20);
-//var css = rgb.toCSS()
-//console.log(css)
