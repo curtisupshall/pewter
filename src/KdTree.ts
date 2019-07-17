5aa919552e9ea9b15e666fbb083d8c8949a34ee8
Curtis Upshall <curtisupshall@uvic.ca>
2019-07-16 22:08:57 -0700

    Commit KdTree

---
+++ src/KdTree.ts
@@ -0,0 +1,69 @@
+export class KdTreeNode<T> {
+    public value: T
+    public vector: number[]
+
+    constructor (value: T, vector: number[]) {
+        this.value = value
+        this.vector = vector
+    }
+}
+
+export class KdTree<T> {
+    public data: KdTreeNode<T>
+    public left: KdTree<T>
+    public right: KdTree<T>
+
+    constructor (k: number, elements: KdTreeNode<T>[], depth: number = 0) {
+        if (elements.length === 0) {
+            return
+        }
+
+        // Determine axis
+        const axis: number = depth % k
+
+        // Sort elements based on axis
+        elements.sort((a: KdTreeNode<T>, b: KdTreeNode<T>) => {
+            return a.vector[axis] - b.vector[axis]
+        })
+
+        // Select the median element
+        const medianIndex: number = Math.ceil(elements.length / 2)
+        this.data = elements[medianIndex]
+        this.left = new KdTree<T>(k, elements.slice(0, medianIndex), depth + 1)
+        this.right = new KdTree<T>(k, elements.slice(medianIndex + 1, elements.length), depth + 1)
+    }
+
+    /**
+     * Return the KdTreeNode in the tree whose vector is nearest
+     * to the given vector.
+     * @param vector The given vector to find the nearest neighbor.
+     * @return
+     */
+    public nearestNeighbor = (vector: number[]) => {
+        let nearestNeighbor: KdTreeNode<T> = this.data
+        let depth: number = 0
+        for(;;) {
+            let nearestDistance: number = this.distance(vector, nearestNeighbor.vector)
+            
+            
+        }
+    }
+
+    /**
+     * Calculate the distance between two vectors.
+     * @param a The first vector.
+     * @param b The second vector.
+     * @return The distance between the two vectors.
+     */
+    private distance = (a: number[], b: number[]): number => {
+        // Vectors must have same distance
+        if (a.length !== b.length) {
+            return null
+        }
+        let sum: number = 0;
+        for (let i: number = 0; i < a.length; i ++) {
+            sum += Math.pow(a[i] - b[i], 2)
+        }
+        return Math.sqrt(sum);
+    }
+}
